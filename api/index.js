0c9600be855ad4d3ac585f928b60880343470279
yesmore <3224266014@qq.com>
2023-01-30 20:58:44 +0800

    chore

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ module.exports = async (req, res) => {
   const gh_sig = req.headers["x-hub-signature"];
   const gh_sig_256 = req.headers["x-hub-signature-256"];
 
-  if (method === "POST") {
+  if (method === "POST" && verifyWebhook.verifyWebhook(body, gh_sig_256)) {
     event.eventSwitch(gh_event, body).then((Msg) => {
       console.log(Msg);
       sendMsg
