091fd7584eda23a9224568662d8a9850925b8fd3
songjunxi <songjunxi@fdsure.com>
2023-02-03 11:09:15 +0800

    3

--- api/bot.js
+++ api/bot.js
@@ -1,5 +1,4 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { setTelegramWebhook } = require("../bot/tg_hook.js");
 
 const tgToken = process.env.TG_TOKEN;
 const tgChatId = process.env.TG_CHAT_ID;
@@ -7,7 +6,7 @@ const tgChatId = process.env.TG_CHAT_ID;
 module.exports = async (req, res) => {
   const bot = new TelegramBot(tgToken, { polling: true });
   try {
-    const { body } = request;
+    const { body } = req;
     if (body.message) {
       const {
         chat: { id },
@@ -16,22 +15,22 @@ module.exports = async (req, res) => {
       const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
       await bot.sendMessage(id, message, { parse_mode: "Markdown" });
 
-      bot.onText(/\/name/, (msg, match) => {
-        bot.sendMessage(msg.chat.id, "yesmore");
-      });
+      // bot.onText(/\/name/, (msg, match) => {
+      //   bot.sendMessage(msg.chat.id, "yesmore");
+      // });
 
-      bot.on("message", (msg) => {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, "'I am alive!'");
-      });
+      // bot.on("message", (msg) => {
+      //   const chatId = msg.chat.id;
+      //   bot.sendMessage(chatId, "'I am alive!'");
+      // });
 
-      bot.on("polling_error", (error) => {
-        console.log(error.code); // => 'EFATAL'
-      });
+      // bot.on("polling_error", (error) => {
+      //   console.log(error.code); // => 'EFATAL'
+      // });
 
-      bot.on("webhook_error", (error) => {
-        console.log(error.code); // => 'EPARSE'
-      });
+      // bot.on("webhook_error", (error) => {
+      //   console.log(error.code); // => 'EPARSE'
+      // });
     }
   } catch (error) {
     // If there was an error sending our message then we
@@ -39,5 +38,5 @@ module.exports = async (req, res) => {
     console.error("Error sending message");
     console.log(error.toString());
   }
-  response.send("OK");
+  res.send("OK");
 };
