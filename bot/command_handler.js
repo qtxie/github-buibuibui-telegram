d623ba8a99f5fd7dc0c892b675ab030caaab704f
yesmore <3224266014@qq.com>
2023-02-04 18:17:15 +0800

    upd: modify geshi

--- bot/command_handler.js
+++ bot/command_handler.js
@@ -17,12 +17,12 @@ module.exports.handleBotRepo = async ({ cmd, id, bot }) => {
   );
 };
 module.exports.handleHelp = async ({ cmd, id, bot }) => {
-  const content = `欢迎使用 Aka 机器人 :>\n\n
-     支持的命令：
-     - /help 查看帮助
-     - /repo 机器人仓库
-     - /s [option] 资源搜索
-     - /start ...
+  const content = `欢迎使用 Aka 机器人 :>\n
+  命令列表：
+    /help 查看帮助
+    /repo 机器人仓库
+    /s [option] 资源搜索
+    /start ...
     `;
   await bot.sendMessage(id, content, message_option);
 };
