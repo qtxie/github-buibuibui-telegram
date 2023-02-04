acfaea11a1be165989c0057ad49a9d9ceb0428ca
yesmore <3224266014@qq.com>
2023-02-04 15:39:58 +0800

    add opt

--- bot/command_handler.js
+++ bot/command_handler.js
@@ -1,15 +1,21 @@
+const message_option = { parse_mode: "Markdown" };
+
 module.exports.handleStart = async ({ cmd, id, bot }) => {
-  await bot.sendMessage(id, "hello hello");
+  await bot.sendMessage(id, "hello hello", message_option);
 };
 module.exports.handleName = async ({ cmd, id, bot }) => {
-  await bot.sendMessage(id, "yesmore");
+  await bot.sendMessage(id, "yesmore", message_option);
 };
 module.exports.handleSearch = async ({ cmd, action, id, bot }) => {
-  await bot.sendMessage(id, `search: ${action}`);
+  await bot.sendMessage(id, `search: ${action}`, message_option);
 };
 module.exports.handleUnRegistedCmd = async ({ cmd, id, bot }) => {
-  await bot.sendMessage(id, "未知命令");
+  await bot.sendMessage(
+    id,
+    "![ememe](https://avatars.githubusercontent.com/u/89140804?s=400&u=51e421ccfe9f90b76625788fb80457eafdac7f86&v=4)",
+    message_option
+  );
 };
 module.exports.handleHelp = async ({ cmd, id, bot }) => {
-  await bot.sendMessage(id, "机器人说明...");
+  await bot.sendMessage(id, "机器人说明...", message_option);
 };
