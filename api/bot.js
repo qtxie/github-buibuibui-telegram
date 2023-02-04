d220244d665d714e18e2e1af336519b0e364dd1a
yesmore <3224266014@qq.com>
2023-02-04 13:50:08 +0800

    15

--- api/bot.js
+++ api/bot.js
@@ -2,56 +2,58 @@ const TelegramBot = require("node-telegram-bot-api");
 const TgOpenApi = require("../utils/tg_open_apis.js");
 
 module.exports = async (request, response) => {
-  const tgToken = process.env.TG_TOKEN;
-  const tgChatId = process.env.TG_CHAT_ID;
-  const bot = new TelegramBot(tgToken);
+  try {
+    const tgToken = process.env.TG_TOKEN;
+    const tgChatId = process.env.TG_CHAT_ID;
+    const bot = new TelegramBot(tgToken);
 
-  const { body } = request;
+    const { body } = request;
 
-  if (body.message) {
-    const {
-      chat: { id },
-      text,
-    } = body.message;
+    if (body.message) {
+      const {
+        chat: { id },
+        text,
+      } = body.message;
 
-    // if (text === "/name") {
-    //   await bot.sendMessage(id, "yesmore111");
-    // }
+      // if (text === "/name") {
+      //   await bot.sendMessage(id, "yesmore111");
+      // }
 
-    // bot.onText(/\/name/, (msg, match) => {
-    //   bot.sendMessage(msg.chat.id, "yesmore");
-    // });
+      // bot.onText(/\/name/, (msg, match) => {
+      //   bot.sendMessage(msg.chat.id, "yesmore");
+      // });
 
-    bot.onText(/\/name (.+)/, (msg, match) => {
-      const chatId = msg.chat.id;
-      const resp = match[1];
-      // bot.sendMessage(chatId, resp);
-      TgOpenApi.sendMsg(resp)
-        .then(() => {
-          response.status(201).send({ status: "ok" });
-        })
-        .catch((err) => {
-          console.log(err);
-          response.status(err.response.status).send(err.response.statusText);
-        });
-    });
+      bot.onText(/\/name (.+)/, (msg, match) => {
+        const chatId = msg.chat.id;
+        const resp = match[1];
+        // bot.sendMessage(chatId, resp);
+        TgOpenApi.sendMsg(resp)
+          .then(() => {
+            response.status(201).send({ status: "ok" });
+          })
+          .catch((err) => {
+            console.log(err);
+            response.status(err.response.status).send(err.response.statusText);
+          });
+      });
 
-    bot.on("message", (msg) => {
-      const chatId = msg.chat.id;
-      bot.sendMessage(chatId, "'I am alive!'");
-    });
+      bot.on("message", (msg) => {
+        const chatId = msg.chat.id;
+        bot.sendMessage(chatId, "'I am alive!'");
+      });
 
-    // bot.on("polling_error", (error) => {
-    //   console.log(error.code); // => 'EFATAL'
-    // });
+      // bot.on("polling_error", (error) => {
+      //   console.log(error.code); // => 'EFATAL'
+      // });
 
-    // bot.on("webhook_error", (error) => {
-    //   console.log(error.code); // => 'EPARSE'
-    // });
+      // bot.on("webhook_error", (error) => {
+      //   console.log(error.code); // => 'EPARSE'
+      // });
 
-    const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
-    await bot.sendMessage(id, message);
-  }
+      const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+      await bot.sendMessage(id, message);
+    }
+  } catch (error) {}
 
   response.send("OK");
 };
